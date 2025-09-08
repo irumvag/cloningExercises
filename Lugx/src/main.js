a23d816bcb7335f074cf27ce604d623b85809cff
unknown <irumvagadanaclet@gmai.com>
2025-09-08 11:52:39 +0200

    tailwindcss learning

--- Lugx/src/main.js
+++ Lugx/src/main.js
@@ -1,24 +1,19 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+let btn=document.getElementById('navbtn');
+let navbar=document.getElementById('navbar');
+btn.addEventListener('click',()=>{
+  navbar.classList.toggle('hidden');
+  console.log('clicked');
+});
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+let scl=document.getElementById('navsticky');
+window.addEventListener('scroll',()=>{
+  if(window.scrollY>50){
+    scl.classList.add('bg-blue-600');
+    console('allived');
+  }
+  else{
+    scl.classList.remove('bg-blue-600');
+  }
+});
 
-setupCounter(document.querySelector('#counter'))
+
